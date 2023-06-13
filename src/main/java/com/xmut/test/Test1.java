f80c3208150a8251858e17a2a4827eb41ce873de
peimingxu <775685661@qq.com>
2023-06-13 14:05:14 +0800

    first commit

---
+++ src/main/java/com/xmut/test/Test1.java
@@ -0,0 +1,11 @@
+package com.xmut.test;
+
+/**
+ * @author
+ * @date: 2023/6/13
+ **/
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
