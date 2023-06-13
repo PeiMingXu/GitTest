a810453a95116e874d174946a47d2858917f3f20
peimingxu <775685661@qq.com>
2023-06-13 14:08:54 +0800

    second commit

--- src/main/java/com/xmut/test/Test1.java
+++ src/main/java/com/xmut/test/Test1.java
@@ -7,5 +7,6 @@ package com.xmut.test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
